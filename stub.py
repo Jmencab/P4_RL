bdfdc31efa5cfb6d5228d5062ecb7cd3fd5311e8
jessijzhao <jessijzhao@gmail.com>
2018-04-28 21:39:43 -0400

    a mess

--- stub.py
+++ stub.py
@@ -2,6 +2,8 @@
 import numpy as np
 import numpy.random as npr
 import pygame as pg
+from collections import defaultdict
+
 
 from SwingyMonkey import SwingyMonkey
 
@@ -12,7 +14,7 @@ class Learner(object):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
-        self.binsize = {'height' : 40, 'width': 30, 'vel' : 5 }
+        self.binsize = {'height' : 10, 'width': 20, 'vel' : 10 }
         self.iterations = iterations
         self.epsilon = epsilon
         self.Q = np.zeros((400/self.binsize['height'], 600/self.binsize['width'], int(100/self.binsize['vel']), 2))
@@ -20,29 +22,33 @@ class Learner(object):
     def reset(self, iterations, epsilon):
         self.last_state  = None
         self.last_action = None
-        # self.last_reward = None
-        self.iterations = iterations
+        self.last_reward = None
         self.epsilon = epsilon / 1.01
-        # self.Q = np.zeros((400/self.binsize['height'], 600/self.binsize['width'], 25, 2))
 
     def indices(self, state):
-        height = int((state['tree']['top'] - state['monkey']['top']) / self.binsize['height'])
+        print state['monkey']['vel']
+        height = int((state['monkey']['top'] - state['tree']['top'] + 200) / self.binsize['height'])
         width = int(state['tree']['dist'] / self.binsize['width'])
         vel = int((state['monkey']['vel'] + 50) / self.binsize['vel'])
-        return height, width, vel
+        if vel > 10 :
+            vel = 8
+        elif vel < 0:
+            vel = 0
+        return (height, width, vel)
 
     def action_callback(self, state):
 
-        # You might do some learning here based on the current state and the last state.
         new_state = self.indices(state)
+
         self.iterations += 1
+
         if npr.random() < .1:
             if npr.random() < .5:
                 new_action = 0
             else:
                 new_action = 1
         else:
-            new_action = np.argmax(self.Q[new_state])
+            new_action = np.argmax(new_state)
 
         if (self.last_state is None and
             self.last_action is None and
@@ -58,9 +64,24 @@ class Learner(object):
         self.last_action = new_action
         self.last_state  = new_state
 
-        self.Q[last_state][last_action] += self.epsilon * (last_reward + (max(self.Q[new_state])) - self.Q[last_state][last_action])
-        print self.Q.max()
+        self.Q[last_state[0]][last_state[1]][last_state[2]][last_action] += self.epsilon * (last_reward + (max(self.Q[new_state[0]][new_state[1]][new_state[2]])) - self.Q[last_state[0]][last_state[1]][last_state[2]][last_action])
+        # print self.Q[last_state[0]][last_state[1]][last_state[2]][last_action] #self.Q.max()
         return self.last_action
+        if (self.last_state is not None and
+            self.last_action is not None and
+            self.last_reward is not None):
+            s = self.last_state
+            a = self.last_action
+            r = self.last_reward
+            sn = new_state
+
+            self.Q[s[0]][s[1]][s[2]][a] += self.epsilon * (r + (max(self.Q[sn[0]][sn[1]][sn[2]])) - self.Q[s[0]][s[1]][s[2]][a])
+
+        self.last_action = new_action
+        self.last_state = new_state
+
+        print self.Q.max()
+        return new_action
 
     def reward_callback(self, reward):
         '''This gets called so you can see what reward you get.'''
@@ -97,7 +118,7 @@ def run_games(learner, hist, iters = 100, t_len = 100):
 if __name__ == '__main__':
 
 	# Select agent.
-	agent = Learner(5, 0.5)
+	agent = Learner(5, 0.4)
 
 	# Empty list to save history.
 	hist = []
